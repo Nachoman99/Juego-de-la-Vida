89aae1e49e146c96547c370c88a3790284aeccbd
Kevin Trejos <Kevin Trejos@KTC>
2018-10-29 19:12:11 -0600

    Se crearon las clase Grid y cell

---
+++ JuegoDeLaVida/src/juegodelavida/Cell.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegodelavida;
+
+/**
+ * This class represents the grid of the game
+ * 
+ *@version 29/10/2018
+ * @author Kevin Trejos
+ */
+public class Cell {
+    
+}
