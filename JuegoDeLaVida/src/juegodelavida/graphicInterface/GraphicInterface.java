7bac2b8fbdf1c84c82669027f222877f2419aedd
Kevin Trejos <Kevin Trejos@KTC>
2018-11-10 13:41:55 -0600

    Acomodando

--- JuegoDeLaVida/src/juegodelavida/graphicInterface/GraphicInterface.java
+++ JuegoDeLaVida/src/juegodelavida/graphicInterface/GraphicInterface.java
@@ -106,20 +106,21 @@ public class GraphicInterface {
     }
     
     public int generations(int generationNumber, String[][] generation1){
+        int desition;
         if(generationNumber == 20){
             String[] buttons ={"Ver generaciones anteriores", "Terminar juego"};
-            int desition = JOptionPane.showOptionDialog(null, getStringMatrix(generation1) + "Se llego al maximo de generaciones, selecciones ver generaciones o terminar el juego", "GENERACIONES", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[0]);
+            desition = JOptionPane.showOptionDialog(null, getStringMatrix(generation1) + "Se llego al maximo de generaciones, selecciones ver generaciones o terminar el juego", "GENERACIONES", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[0]);
             if (desition == 2) {
                 JOptionPane.showMessageDialog(null, "Muchas gracias por jugar");
+                return desition;
             }
-            return desition; 
+             
         }else{
            String[] buttons ={"Siguiente", "Ver generaciones anteriores"};
-            int desition = JOptionPane.showOptionDialog(null, getStringMatrix(generation1) + "Seleccione alguna opción", "GENERACIONES", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[0]);  
+            desition = JOptionPane.showOptionDialog(null, getStringMatrix(generation1) + "Seleccione alguna opción", "GENERACIONES", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[0]);  
             return desition; 
         }
-       
-        
+        return desition;
     }
     /*
     public int generations(String[][] generation1){
